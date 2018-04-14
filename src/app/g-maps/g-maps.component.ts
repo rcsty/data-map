6ddb7ccacc3787d091166d6f6ff1944dbc55401c
rcsty <raphael.castay@gmail.com>
2018-04-15 00:13:20 +0200

    Add google maps

--- src/app/g-maps/g-maps.component.ts
+++ src/app/g-maps/g-maps.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { GMapService } from './shared/g-map.service';
 
 @Component({
   selector: 'app-g-maps',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GMapsComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('gmap') gMapElement: ElementRef;
+
+  constructor(private gMapService: GMapService) { }
 
   ngOnInit() {
+    this.gMapService.initialize(new google.maps.Map(this.gMapElement.nativeElement, {
+      center: new google.maps.LatLng(43.6006866, 1.439026), // Toulouse, France
+      zoom: 12.5,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }));
   }
 
 }
--- src/app/g-maps/shared/g-map.service.ts
+++ src/app/g-maps/shared/g-map.service.ts
@@ -3,6 +3,12 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GMapService {
 
+  map: google.maps.Map;
+
   constructor() { }
+
+  public initialize(map) {
+    this.map = map;
+  }
 
 }
